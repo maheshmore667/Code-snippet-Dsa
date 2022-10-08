6884eea9137d50516064f56923ddcbbcd760680a
Mahesh More <maheshmore667@gmai.com>
2022-10-08 11:54:39 +0530

    set stl in cpp

---
+++ Stl/set.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main() {
+    set<int> s;
+    //set always has unique and sorted element
+    s.insert(5);
+    s.insert(5);
+    s.insert(1);
+    s.insert(2);
+    s.insert(4);
+    //insert -> time complexity O(logn)
+
+    for(int i : s){
+        cout << i << " ";
+    } cout << endl;
+
+    cout << "size of the set :" << s.size() << endl;
+    cout << "Is set is empty : " << s.empty()<< endl;
+
+    set<int> :: iterator it = s.begin();
+    it++;
+
+    s.erase(it);
+
+    cout << "set after erase" << endl;
+     for(int i : s){
+        cout << i << " ";
+    } cout << endl;
+
+
+
+}
