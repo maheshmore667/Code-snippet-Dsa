0594cd93c7ec6e42491fd6346af3db76eb7ccbb3
Mahesh More <maheshmore667@gmai.com>
2022-12-18 21:40:39 +0530

    test case addition code improved

--- Recursion/quickSortRecursion.cpp
+++ Recursion/quickSortRecursion.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int partition(int *nums, int start, int end) {
     int count = 0;
-    for (int i = start+1; i< end; i++) {
+    for (int i = start+1; i<= end; i++) {
         if(nums[i] <= nums[start]){
             count++;
         }
@@ -13,14 +13,17 @@ int partition(int *nums, int start, int end) {
     int startPointer = start;
     int endPointer = end;
 
-    while(startPointer <= endPointer ) {
-        while(nums[startPointer] < nums[correctIndex]){
+    while(startPointer < correctIndex && endPointer > correctIndex ) {
+        while(nums[startPointer] <= nums[correctIndex]){
             startPointer++;
         }
         while(nums[endPointer] > nums[correctIndex]){
             endPointer--;
         }
-        swap(nums[startPointer++], nums[endPointer--]);
+        if(startPointer < correctIndex && endPointer > correctIndex) {
+             swap(nums[startPointer++], nums[endPointer--]);
+        }
+       
     }
     return correctIndex;
 }
@@ -34,14 +37,14 @@ void quickSortRecursionFunc(int *nums, int start, int end)
 
     int correctIndex = partition(nums, start, end);
     
-    quickSortRecursionFunc(nums, start, correctIndex);
+    quickSortRecursionFunc(nums, start, correctIndex-1);
     quickSortRecursionFunc(nums, correctIndex+1, end);
 }
 
 int main() {
-    int nums[] = {14, 8,9, 23, 10, 11, 2};
-    quickSortRecursionFunc(nums, 0, 7);
-    for(int i=0; i < 7; i++) {
+    int nums[] = {4,2,5,1,3};
+    quickSortRecursionFunc(nums, 0, 4);
+    for(int i=0; i < 5; i++) {
         cout << nums[i] << " ";
     }
     return 0;
