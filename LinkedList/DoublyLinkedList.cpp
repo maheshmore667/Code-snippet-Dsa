746b0fe608d6bb09fbace639da7b4af1941530d5
Mahesh More <maheshmore667@gmai.com>
2023-01-16 21:08:47 +0530

    doubly linked list basic code implemented

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -41,11 +41,60 @@ int getLengthOfLinkedList(Node* &Head) {
     return length;
 }
 
+void insertAtHead(Node* &Head, int data) {
+    Node* newNode = new Node(data);
+    newNode -> next = Head;
+    Head -> prev = newNode;
+    Head = newNode;
+}
+
+void insertAtTail(Node* &Tail, int data) {
+    Node* newNode = new Node(data);
+    Tail -> next = newNode;
+    newNode -> prev = Tail;
+    Tail = newNode;
+}
+
+void inserAtPosition(Node* &Head, Node* &Tail, int data, int position) {
+
+    if(position == 1) {
+        insertAtHead(Head, data);
+        return;
+    }
+    int cnt = 1;
+    Node* temp = Head;
+    while(cnt != position-1) {
+        temp = temp -> next;
+        cnt++;
+    }
+    if(temp->next == NULL) {
+        insertAtTail(Tail, data);
+        return;
+    }
+    Node* newNode = new Node(data);
+    Node* frwdNode;
+    frwdNode = temp -> next;
+    temp -> next = newNode;
+    newNode -> prev = temp;
+    newNode -> next = frwdNode;
+    frwdNode -> prev = newNode;
+
+}
+
 int main() {
 
     Node* newNode = new Node(10) ;
     Node* Head;
+    Node* Tail;
     Head = newNode;
+    Tail = newNode;
+    insertAtHead(Head, 9);
+    insertAtHead(Head, 8);
+    insertAtHead(Head, 7);
+    insertAtTail(Tail, 11);
+    insertAtTail(Tail, 12);
+    insertAtTail(Tail, 17);
+    inserAtPosition(Head, Tail, 20, 1);
     printLinkedList(Head);
     cout << "Length of Linked List : " << getLengthOfLinkedList(Head);
     return 0;
