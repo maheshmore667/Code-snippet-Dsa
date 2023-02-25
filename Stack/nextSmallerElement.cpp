f5e612018bc9073e6a2b8c69f6c642fbb1177b8d
Mahesh More <maheshmore667@gmai.com>
2023-02-25 15:13:51 +0530

    next smaller element

---
+++ Stack/nextSmallerElement.cpp
@@ -0,0 +1,16 @@
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for(int i= n-1; i>=0; i--) {
+        int num = arr[i];
+        while(s.top() >= num) {
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(num);
+    }
+    return ans;
+}
