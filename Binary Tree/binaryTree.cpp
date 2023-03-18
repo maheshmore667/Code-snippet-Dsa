8da9fca9ce9e8579fd3732ed0d0cf5856d2d6450
Mahesh More <maheshmore667@gmai.com>
2023-03-19 00:10:03 +0530

    created a binary tree using level order traversal data

--- Binary Tree/binaryTree.cpp
+++ Binary Tree/binaryTree.cpp
@@ -123,21 +123,55 @@ void reverseOrderTraversal(Node* root) {
     }
 }
 
+void createbinaryTreeFromLevelOrder(Node* &root) {
+    cout << "Enter the data for the root :" << endl;
+    int rootData;
+    cin >> rootData;
+    root = new Node(rootData);
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        cout << "Enter the data to the left of : " << temp -> data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1) {
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter the data to the right of : " << temp -> data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main() {
     Node* root = NULL;
-    root = createBinaryTree(root);
+    createbinaryTreeFromLevelOrder(root);
+    levelOrderTraversal(root);
+    /* root = createBinaryTree(root);
     levelOrderTraversal(root);
     cout << "Reverse Level Order Traversal" << " " << endl;
-    reverseOrderTraversal(root);
+    reverseOrderTraversal(root); */
 
-    cout << "inorder traversal is : " << endl;
+    /* cout << "inorder traversal is : " << endl;
     inorder(root);
     cout << endl;
     cout << "preorder traversal is : " << endl;
     preorder(root);
     cout << endl;
     cout << "postorder traversal is : " << endl;
-    postorder(root);
+    postorder(root); */
 
 
     //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
